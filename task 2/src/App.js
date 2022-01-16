8ff0e32ec56a88fb7f0c54015e1a4301b4ba3093
Pragati Khandelwal <96972709+Pragatik19@users.noreply.github.com>
2022-01-16 16:23:07 +0530

    Add files via upload

---
+++ task 2/src/App.js
@@ -0,0 +1,41 @@
+import Users from "./cards";
+import "./styles.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: true };
+    this.showUsers = this.showUsers.bind(this);
+  }
+
+  showUsers() {
+    document.getElementById("main").style.display = "flex";
+    const source = "https://reqres.in/api/users?page=1";
+    fetch(source)
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ users_data: users.data, loading: false });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="container-fluid">
+            <h1>..</h1>
+            <button onClick={this.showUsers}>Get Users</button>
+          </div>
+        </nav>
+
+        <Users loading={this.state.loading} users={this.state.users_data} />
+      </>
+    );
+  }
+}
+
+export default App;
